045c7742e74a1b887216699eba16a1aac1f240d8
mehedi <mehedihasan712277@gmail.com>
2026-04-16 22:16:42 +0600

    feat: removed demo controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 
 import userRoutes from "./user.routes";
 import uploadRoutes from "./upload.routes";
-import clientRoutes from "./client.routes";
 import surahRoutes from "./surah.routes";
 import translationRoutes from "./translation.routes";
 import surahNameRoutes from "./surah_name.routes";
@@ -11,7 +10,6 @@ const router = Router();
 
 router.use("/auth", userRoutes);
 router.use("/uploads", uploadRoutes);
-router.use("/clients", clientRoutes);
 router.use("/surahs", surahRoutes);
 router.use("/translations", translationRoutes);
 router.use("/surahnames", surahNameRoutes);
